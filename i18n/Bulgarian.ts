5aec0654134a4ea1dac559ea50780c1215ca645c
Petarkir2000 <37194471+Petarkir2000@users.noreply.github.com>
2020-04-16 15:17:15 +0300

    Create Bulgarian.ts
    To translate the firmware flasher

---
+++ i18n/Bulgarian.ts
@@ -0,0 +1,171 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS><TS version="2.0">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../airrohr-flasher.py" line="54"/>
+        <source>Loading firmware list...</source>
+        <translation>Зареждане на списък с фърмуери</translation>
+    </message>
+    <message>
+        <location filename="../airrohr-flasher.py" line="161"/>
+        <source>No boards found</source>
+        <translation>Няма намерени контролери</translation>
+    </message>
+    <message>
+        <location filename="../airrohr-flasher.py" line="161"/>
+        <source>Have you installed &lt;a href=&quot;{drivers_url}&quot;&gt;the drivers&lt;/a&gt;?</source>
+        <translation>Инсталирахте ле &lt;a href=&quot;{drivers_url}&quot;&gt;драйверите&lt;/a&gt;? </translation>
+    </message>
+    <message>
+        <location filename="../airrohr-flasher.py" line="170"/>
+        <source>Others...</source>
+        <translation>Други...</translation>
+    </message>
+    <message>
+        <location filename="../airrohr-flasher.py" line="280"/>
+        <source>No device selected.</source>
+        <translation>Не е избрано устройство...</translation>
+    </message>
+    <message>
+        <location filename="../airrohr-flasher.py" line="203"/>
+        <source>No version selected.</source>
+        <translation>Не е избрана версия</translation>
+    </message>
+    <message>
+        <location filename="../airrohr-flasher.py" line="223"/>
+        <source>Invalid version / file does not exist</source>
+        <translation>Невалидна версия / файлът не е намерен</translation>
+    </message>
+    <message>
+        <location filename="../airrohr-flasher.py" line="228"/>
+        <source>Work in progess...</source>
+        <translation>В ход...</translation>
+    </message>
+    <message>
+        <location filename="../airrohr-flasher.py" line="255"/>
+        <source>Downloading...</source>
+        <translation>Сваляне...</translation>
+    </message>
+    <message>
+        <location filename="../airrohr-flasher.py" line="295"/>
+        <source>Connecting...</source>
+        <translation>Свързване...</translation>
+    </message>
+    <message>
+        <location filename="../airrohr-flasher.py" line="300"/>
+        <source>Connected. Chip type: {chip_type}</source>
+        <translation>Свързан. Чип тип: {chip_type}</translation>
+    </message>
+    <message>
+        <location filename="../airrohr-flasher.py" line="272"/>
+        <source>Erasing complete!</source>
+        <translation>Изтриването завърши!</translation>
+    </message>
+    <message>
+        <location filename="../airrohr-flasher.py" line="284"/>
+        <source>Erasing in progress...</source>
+        <translation>Процес на изтриване...</translation>
+    </message>
+    <message>
+        <location filename="../airrohr-flasher.py" line="318"/>
+        <source>Writing at 0x{address:08x}...</source>
+        <translation>Запис на 0x{address:08x}...</translation>
+    </message>
+    <message>
+        <location filename="../airrohr-flasher.py" line="329"/>
+        <source>Finished in {time:.2f} seconds. Sensor ID: {sensor_id}</source>
+        <translation>Завърши за {time:.2f} секунди. ID на сензор: {sensor_id}</translation>
+    </message>
+    <message>
+        <location filename="../gui/mainwindow.py" line="230"/>
+        <source>Sensor.Community - Airrohr Flasher (v{version})</source>
+        <translation>Sensor.Community - Airrohr Flasher (v{version})</translation>
+    </message>
+    <message>
+        <location filename="../gui/mainwindow.py" line="231"/>
+        <source>Firmware Version:</source>
+        <translation>Версия на фърмуера:</translation>
+    </message>
+    <message>
+        <location filename="../gui/mainwindow.py" line="232"/>
+        <source>Upload</source>
+        <translation>Качване</translation>
+    </message>
+    <message>Флаш изтриване
+        <location filename="../gui/mainwindow.py" line="233"/>
+        <source>Erase Flash</source>
+        <translation>Флаш изтриване</translation>
+    </message>
+    <message>
+        <location filename="../gui/mainwindow.py" line="234"/>
+        <source>Expert mode</source>
+        <translation>Експертен режим</translation>
+    </message>
+    <message>
+        <location filename="../gui/mainwindow.py" line="235"/>
+        <source>Board:</source>
+        <translation>Контролер:</translation>
+    </message>
+    <message>
+        <location filename="../gui/mainwindow.py" line="236"/>
+        <source>Baudrate:</source>
+        <translation>Скорост на обмен</translation>
+    </message>
+    <message>
+        <location filename="../gui/mainwindow.py" line="237"/>
+        <source>Flashing</source>
+        <translation>Флашване</translation>
+    </message>
+    <message>
+        <location filename="../gui/mainwindow.py" line="238"/>
+        <source>Double-click to open configuration page.</source>
+        <translation>Щракнете двукратно, за да отворите конфигурационната страница</translation>
+    </message>
+    <message>
+        <location filename="../gui/mainwindow.py" line="239"/>
+        <source>Sensors detected in local network:</source>
+        <translation>Сензори открити в локалната мрежа</translation>
+    </message>
+    <message>
+        <location filename="../gui/mainwindow.py" line="240"/>
+        <source>Refresh</source>
+        <translation>Обновяване</translation>
+    </message>
+    <message>
+        <location filename="../gui/mainwindow.py" line="241"/>
+        <source>Discovery</source>
+        <translation>Търсене в мрежата</translation>
+    </message>
+    <message>
+        <location filename="../gui/mainwindow.py" line="242"/>
+        <source>In case of sensor issues, Serial Monitor can be used to review logs sent by the sensor over USB cable.</source>
+        <translation> В случай на проблеми със сензора, Сериен монитор може да се използва за преглед на регистрационни файлове, изпратени от сензора по USB кабел.</translation>
+    </message>
+    <message>
+        <location filename="../gui/mainwindow.py" line="243"/>
+        <source>Connect</source>
+        <translation>Свързване</translation>
+    </message>
+    <message>
+        <location filename="../gui/mainwindow.py" line="244"/>
+        <source>Serial Monitor</source>
+        <translation>Сериен монитор</translation>
+    </message>
+    <message>
+        <location filename="../gui/mainwindow.py" line="245"/>
+        <source>&lt;b&gt;Sensor.Community Airrohr Flasher&lt;/b&gt;&lt;br/&gt;Build {build_id}</source>
+        <translation>&lt;b&gt;Sensor.Community Airrohr Flasher&lt;/b&gt;&lt;br/&gt;Build {build_id}</translation>
+    </message>
+    <message>
+        <location filename="../gui/mainwindow.py" line="246"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Developed by &lt;a href=&quot;https://inf.re/&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0000ff;&quot;&gt;Piotr Dobrowolski&lt;/span&gt;&lt;/a&gt;.&lt;/p&gt;&lt;p&gt;This software is released under the terms of MIT license. No warranty is provided.&lt;/p&gt;&lt;p&gt;For newest release see: &lt;a href=&quot;https://d.inf.re/luftdaten/&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0000ff;&quot;&gt;https://d.inf.re/luftdaten/&lt;/span&gt;&lt;/a&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Създаден от &lt;a href=&quot;https://inf.re/&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0000ff;&quot;&gt;Piotr Dobrowolski&lt;/span&gt;&lt;/a&gt;.&lt;/p&gt;&lt;p&gt;This software is released under the terms of MIT license. No warranty is provided.&lt;/p&gt;&lt;p&gt;For newest release see: &lt;a href=&quot;https://d.inf.re/luftdaten/&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0000ff;&quot;&gt;https://d.inf.re/luftdaten/&lt;/span&gt;&lt;/a&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <location filename="../gui/mainwindow.py" line="247"/>
+        <source>About</source>
+        <translation>Относно</translation>
+    </message>
+</context>
+</TS>
